129c46f76189cb5bf2dbb0bace7595a4d585ae86
Mokbul Hossain <abdullahalmokbul624@gmail.com>
2022-07-28 12:53:14 +0600

    Add score panel

--- ShooterPro/src/GamePlay.java
+++ ShooterPro/src/GamePlay.java
@@ -69,32 +69,64 @@ public class GamePlay extends JPanel implements ActionListener {
 		
 		if(play) {
 			// draw shooter 1
-						if(shooter1up)
-							shooter1=new ImageIcon("images/shooter1up.png");	
-						else if(shooter1down)
-							shooter1=new ImageIcon("images/shooter1down.png");
-						else if(shooter1right)
-							shooter1=new ImageIcon("images/shooter1right.png");
-						else if(shooter1left)
-							shooter1=new ImageIcon("images/shooter1left.png");
-							
-						shooter1.paintIcon(this, g, shooter1X, shooter1Y);
+			if(shooter1up)
+				shooter1=new ImageIcon("images/shooter1up.png");	
+			else if(shooter1down)
+				shooter1=new ImageIcon("images/shooter1down.png");
+			else if(shooter1right)
+				shooter1=new ImageIcon("images/shooter1right.png");
+			else if(shooter1left)
+				shooter1=new ImageIcon("images/shooter1left.png");
+				
+			shooter1.paintIcon(this, g, shooter1X, shooter1Y);
+			
+			// draw shooter 2
+			if(shooter2up)
+				shooter2=new ImageIcon("images/shooter2up.png");	
+			else if(shooter2down)
+				shooter2=new ImageIcon("images/shooter2down.png");
+			else if(shooter2right)
+				shooter2=new ImageIcon("images/shooter2right.png");
+			else if(shooter2left)
+				shooter2=new ImageIcon("images/shooter2left.png");
 						
-						// draw shooter 2
-						if(shooter2up)
-							shooter2=new ImageIcon("images/shooter2up.png");	
-						else if(shooter2down)
-							shooter2=new ImageIcon("images/shooter2down.png");
-						else if(shooter2right)
-							shooter2=new ImageIcon("images/shooter2right.png");
-						else if(shooter2left)
-							shooter2=new ImageIcon("images/shooter2left.png");
-									
-						shooter2.paintIcon(this, g, shooter2X, shooter2Y);
+			shooter2.paintIcon(this, g, shooter2X, shooter2Y);
 		}
 		
 		
-	
+		// Score Panel 		
+		g.setColor(Color.white);
+		g.setFont(new Font("serif",Font.BOLD, 15));
+		g.drawString("Scores", 700,30);
+		g.drawString("Player 1:  "+shooter1score, 670,60);
+		g.drawString("Player 2:  "+shooter2score, 670,90);
+		
+		g.drawString("Lives", 700,150);
+		g.drawString("Player 1:  "+shooter1lives, 670,180);
+		g.drawString("Player 2:  "+shooter2lives, 670,210);
+		
+		if(shooter1lives == 0)
+		{
+			g.setColor(Color.white);
+			g.setFont(new Font("serif",Font.BOLD, 60));
+			g.drawString("Game Over", 200,300);
+			g.drawString("Player 2 Won!", 180,380);
+			play = false;
+			g.setColor(Color.white);
+			g.setFont(new Font("serif",Font.BOLD, 30));
+			g.drawString("(Space to Restart)", 230,430);
+		}
+		else if(shooter2lives == 0)
+		{
+			g.setColor(Color.white);
+			g.setFont(new Font("serif",Font.BOLD, 60));
+			g.drawString("Game Over", 200,300);
+			g.drawString("Player 1 Won!", 180,380);
+			g.setColor(Color.white);
+			g.setFont(new Font("serif",Font.BOLD, 30));
+			g.drawString("(Space to Restart)", 230,430);
+			play = false;
+		}
 		
 		
 		
@@ -122,7 +154,7 @@ public class GamePlay extends JPanel implements ActionListener {
 				shooter1right = false;
 				shooter1left = false;
 				shooter1down = false;
-				shooter1up = true;	
+				shooter1up = true;
 				
 				shooter2X = 400;
 				shooter2Y = 530;	
